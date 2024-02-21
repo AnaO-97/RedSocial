0071c49fa304d771b8d94744d49fbef8af2f6c70
ana.orozco@autonoma.edu.co <ana.orozco@autonoma.edu.co>
2024-02-21 17:24:23 -0500

    First commit

---
+++ server/src/app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, ()=>{
+    console.log("Server running on port 3000 XD");
+});git remote add origin https://github.com/AnaO-97/RedSocial.git
